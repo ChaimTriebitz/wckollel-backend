650ccd3847f3daa2eca081f8b08062bedf74e8ab
chaim <chaim.triebitz@bhbsdventures.com>
2024-12-08 13:48:55 +0200

    sunday

--- models/Schedules.js
+++ models/Schedules.js
@@ -2,20 +2,28 @@ const mongoose = require('mongoose');
 
 const Schedules = new mongoose.Schema({
    date: {
+      type: Date,
+      required: true,
+   },
+   time_frame: {
+      type: String,
+      required: true,
+   },
+   event_location_1: {
       type: String,
    },
-   shacharit: {
+   event_location_2: {
       type: String,
    },
-   mincha: {
+   event_location_3: {
       type: String,
    },
-   arvit: {
+   event_location_4: {
       type: String,
    },
-   shiurim: {
+   event_location_5: {
       type: String,
-   }
+   },
 })
 
 module.exports = mongoose.model('Schedules', Schedules);
